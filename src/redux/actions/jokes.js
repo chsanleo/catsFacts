fc233828624b32c1c8607a50c4fe70c087af2bea
Christian <chsanleo@gmail.com>
2022-06-12 13:00:55 +0200

    create actions jokes

---
+++ src/redux/actions/jokes.js
@@ -0,0 +1,23 @@
+import { ADD_JOKES } from "../types/jokes.js";
+import store from '../store.js';
+
+export const addJokes = async (events) => {
+    try {
+        store.dispatch({
+            type: ADD_JOKES,
+            payload: events
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+export const delJokes = async () => {
+    try {
+        store.dispatch({
+            type: ADD_JOKES,
+            payload: ''
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
