d73393c14912ae6db9c71939b307f6e020ccdb34
Christian <chsanleo@gmail.com>
2022-06-12 12:08:11 +0200

    add 404 to routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.scss';
 
-
+import Page404 from './views/404/404.jsx';
 
 function App() {
   return (
     <Router>
       <Switch>
-
+        <Route component={Page404} />
       </Switch>
     </Router>
   );
