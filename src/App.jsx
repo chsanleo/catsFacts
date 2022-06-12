09271a202de5451684f448f28320f48ab5223747
Christian <chsanleo@gmail.com>
2022-06-12 12:05:08 +0200

    change js by jsx
    and ini file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import './App.scss';
+
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
