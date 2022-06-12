692780683d87f9cb0b1c0fba441a6700f0c883f2
Christian <chsanleo@gmail.com>
2022-06-12 14:12:38 +0200

    add landing page
    change jokepage for landing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import './App.scss';
 
 import Page404 from './views/404/404.jsx';
 import Header from './components/header/header.jsx';
-import FactsList from './views/factsList/factsList.jsx';
+import Landing from './views/landing/landing.jsx';
+import Jokes from './views/jokes/jokes.jsx';
 import About from './views/about/about.jsx';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<FactsList />} />
+        <Route path='/'   element={<Landing />} />
+        <Route path='/jokes' element={<Jokes />} />
         <Route path='/about' element={<About />} />
         <Route path = '*' element={<Page404 />} />
       </Routes>
