a1f2c449ec3a302d8822fc637173fd296547767b
Christian <chsanleo@gmail.com>
2022-06-12 14:15:29 +0200

    create component to show list of jokes

---
+++ src/components/jokesList/jokesList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+class JokesList extends React.Component {
+    render() {
+        return(<div>
+            
+           { this.props.jokesList?.map(jokes => ( 
+              <div> {jokes.setup} : {jokes.delivery } </div>
+              ))
+            }
+
+        </div>);
+    };
+
+};
+
+const mapStateToProps = ({jokes}) => ({jokesList: jokes.jokesList})
+export default connect(mapStateToProps)(JokesList);
