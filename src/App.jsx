1bdb6f4d7c3950370b382903b22e8ebbfb9abd72
Christian <chsanleo@gmail.com>
2022-06-12 13:41:04 +0200

    add about to routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.scss';
 import Page404 from './views/404/404.jsx';
 import Header from './components/header/header.jsx';
 import FactsList from './views/factsList/factsList.jsx';
+import About from './views/about/about.jsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<FactsList />} />
+        <Route path='/about' element={<About />} />
         <Route path = '*' element={<Page404 />} />
       </Routes>
     </Router>
