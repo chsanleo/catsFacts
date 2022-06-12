651b86395f4ccb907767d52b660b08daaac38a95
Christian <chsanleo@gmail.com>
2022-06-12 13:16:47 +0200

    create jokes service
    link with redux

---
+++ src/services/jokesService.js
@@ -0,0 +1,12 @@
+import { jokesRepository } from '../repositories/jokesRepository';
+import {addJokes} from '../redux/actions/jokes.js';
+
+export const jokesService ={
+    getJokes
+};
+
+function getJokes(){
+    jokesRepository.getJokes()
+        .then(res => addJokes(res))
+        .catch(error => console.log(error));
+};
