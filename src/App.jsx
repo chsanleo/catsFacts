ad2821ea99506e1114d235eaa33f9511843470c5
Christian <chsanleo@gmail.com>
2022-06-12 12:48:15 +0200

    add fact list and header to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 
 import Page404 from './views/404/404.jsx';
+import Header from './components/header/header.jsx';
+import FactsList from './views/factsList/factsList.jsx';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
+        <Route path='/' element={<FactsList />} />
         <Route path = '*' element={<Page404 />} />
       </Routes>
     </Router>
