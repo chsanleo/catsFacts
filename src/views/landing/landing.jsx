83101c750893217d5ce01d8895566ca0dbdb0ff7
Christian <chsanleo@gmail.com>
2022-06-12 14:14:37 +0200

    add landing page

---
+++ src/views/landing/landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {jokesService} from '../../services/jokesService.js';
+
+class Landing extends React.Component {
+    
+    componentDidMount(){
+        jokesService.getJokes();
+    }
+    render() {
+        return(<div align="center">
+            
+            Welcome
+
+        </div>);
+    };
+};
+export default Landing;
