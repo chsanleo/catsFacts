03139e91f4ed007851791c38539be546bc670079
Christian <chsanleo@gmail.com>
2022-06-12 13:00:41 +0200

    create reducer for jokes
    and init with add and detele

---
+++ src/redux/reducer/jokes.js
@@ -0,0 +1,43 @@
+import { ADD_JOKES } from '../types/jokes.js';
+
+const initialState = {
+    jokesList: ''
+};
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case ADD_JOKES:
+            return {
+                ...state,
+                jokesList: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+/*
+{
+    "error": false,
+    "amount": 3,
+    "jokes": [
+        {
+            "category": "Misc",
+            "type": "twopart",
+            "setup": "Why are cats so good at video games?",
+            "delivery": "They have nine lives.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 255,
+            "safe": true,
+            "lang": "en"
+        },
+        ...
+    }
+*/
