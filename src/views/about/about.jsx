6dbb93a90bacb1cfa7f125f5af418d46a223f677
Christian <chsanleo@gmail.com>
2022-06-12 19:03:58 +0200

    show if exist data from factsData

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -12,7 +12,9 @@ class About extends React.Component {
                 {this.props.jokesList?
                     <div className='api'><li>Thanks to the API: <a href={APIs.JOKES_API} target="_blank" rel="noreferrer">{APIs.JOKES_API}</a></li></div>
                     :<div className={'api '+'elemento'}></div>}
-                <div className={'me '+'elemento'}>
+                {this.props.factsCatList?
+                    <div className='api'><li>Thanks to the API: <a href={APIs.FACTSCAT_API} target="_blank" rel="noreferrer">{APIs.FACTSCAT_API}</a></li></div>
+                    :<div></div>}
                     <h2> About me</h2>
                     <img src="https://avatars2.githubusercontent.com/u/65183792?s=400&u=5e2dc36e1830dd9429313f09a13737d88798c8e1&v=4"alt="Christian Sanchez"/>
                     <a href ="https://github.com/chsanleo"><img src ="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="githubChristian"  target="_blank"  width="40" height="40"/></a>
